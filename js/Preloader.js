598a8b090c0ef611901a66e305e518d63cb8b0c4
aguilanh4-ui <aguilanh4@gmail.com>
2026-08-13 05:17:40 +0200

    Load new RPG character assets

--- js/Preloader.js
+++ js/Preloader.js
@@ -1,35 +1,33 @@
-
 Theodoric.Preloader = function (game) {
-
 	this.background = null;
 	this.preloadBar = null;
-
 	this.ready = false;
 };
 
 Theodoric.Preloader.prototype = {
-
 	preload: function () {
-
-		//	These are the assets we loaded in Boot.js
-		//	A nice sparkly background and a loading progress bar
 		this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
 		this.splash.anchor.setTo(0.5);
-
 		this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloaderBar');
 		this.preloadBar.anchor.setTo(0.5);
-
-		//	This sets the preloadBar sprite as a loader sprite.
-		//	What that does is automatically crop the sprite from 0 to full-width
-		//	as the files below are loaded in.
 		this.load.setPreloadSprite(this.preloadBar);
 
-		//	Here we load the rest of the assets our game needs.
 		this.load.image('playButton', 'assets/images/play.png');
 		this.load.image('flame', 'assets/images/flame.png');
 		this.load.image('sword', 'assets/images/sword.png');
 		this.load.image('levelParticle', 'assets/images/level-particle.png');
 		this.load.image('spellParticle', 'assets/images/spell-particle.png');
+
+		// New pseudo-3D RPG heroes: three identities, each with front/rear/side art.
+		this.load.image('kimi-man-down', 'assets/images/characters/kimi-man-down.svg');
+		this.load.image('kimi-man-up', 'assets/images/characters/kimi-man-up.svg');
+		this.load.image('kimi-man-side', 'assets/images/characters/kimi-man-side.svg');
+		this.load.image('kimi-woman-down', 'assets/images/characters/kimi-woman-down.svg');
+		this.load.image('kimi-woman-up', 'assets/images/characters/kimi-woman-up.svg');
+		this.load.image('kimi-woman-side', 'assets/images/characters/kimi-woman-side.svg');
+		this.load.image('alternative-kim-down', 'assets/images/characters/alternative-kim-down.svg');
+		this.load.image('alternative-kim-up', 'assets/images/characters/alternative-kim-up.svg');
+		this.load.image('alternative-kim-side', 'assets/images/characters/alternative-kim-side.svg');
 
 		this.load.spritesheet('tiles', 'assets/images/tiles.png', 16, 16);
 		this.load.spritesheet('things', 'assets/images/things.png', 16, 16);
@@ -57,26 +55,13 @@ Theodoric.Preloader.prototype = {
 	},
 
 	create: function () {
-
-		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
 		this.preloadBar.cropEnabled = false;
 	},
 
 	update: function () {
-
-		//	You don't actually need to do this, but I find it gives a much smoother game experience.
-		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-		//	You can jump right into the menu if you want and still play the music, but you'll have a few
-		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-		//	it's best to wait for it to decode here first, then carry on.
-		
-		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
-		//	the update function completely.
-		
-		if (this.cache.isSoundDecoded('openingMusic') && this.ready == false)
-			{
-		 	this.ready = true;
-		 	this.state.start('MainMenu');
+		if (this.cache.isSoundDecoded('openingMusic') && this.ready == false) {
+			this.ready = true;
+			this.state.start('MainMenu');
 		}
 	}
 };
